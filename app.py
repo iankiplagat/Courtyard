131ea5a2d77f4eb5cdcf77ba61647b45bbd76a68
Kasparov <ianjkiplagat@gmail.com>
2021-05-03 19:27:37 +0300

    Initial structure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os 
+import os
 
 #Init application
 app = Flask(__name__)
